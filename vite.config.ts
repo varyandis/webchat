9d7663a4062c4a266374d7545284e29cf0ac0463
Александра <varyandis@yandex.ru>
2025-11-09 15:50:19 +0300

    fic build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  server: {
-    port: 3000,
-    strictPort: true
+  server: { port: 3000, strictPort: true },
+  preview: { port: 3000, strictPort: true },
+  build: {
+    rollupOptions: {
+      input: {
+        index:       resolve(__dirname, 'index.html'),
+        login:       resolve(__dirname, 'src/pages/login/login.html'),
+        registration:resolve(__dirname, 'src/pages/registration/registration.html'),
+        chats:       resolve(__dirname, 'src/pages/chats/chats.html'),
+        profile:     resolve(__dirname, 'src/pages/profile/profile.html'),
+        error404:    resolve(__dirname, 'src/pages/error404/error404.html'),
+        error500:    resolve(__dirname, 'src/pages/error500/error500.html'),
+      },
+    },
   },
-  preview: {
-    port: 3000,
-    strictPort: true
-  }
 });
